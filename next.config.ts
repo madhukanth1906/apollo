eb3691b5c295f097e814bf5e753292755f0f95a3
MadhuKanth-M <madhu9940984501@gmail.com>
2026-09-18 21:44:20 +0530

    Fix CORS issue for Flutter Web App

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   turbopack: {},
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  },
 };
 
 export default withPWA({
